2035213529a5911d6e75a78f41fdcb2449e46708
kirillbowkin <kirillshishkin77@gmail.com>
2022-10-16 22:22:50 +0300

    test for ShortUrlGeneratorService

---
+++ src/test/java/kirill/bowkin/urlshortener/service/shortUrlGenerator/ShortUrlGeneratorServiceTest.java
@@ -0,0 +1,43 @@
+package kirill.bowkin.urlshortener.service.shortUrlGenerator;
+
+import kirill.bowkin.urlshortener.service.stringShortener.StringShortener;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.lang.reflect.Field;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+class ShortUrlGeneratorServiceTest {
+
+    ShortUrlGeneratorService shortUrlGeneratorService;
+    private final String hostnameValue = "localhost:8080";
+
+    @Mock
+    private StringShortener stringShortener;
+
+
+    @BeforeEach
+    void setUp() throws NoSuchFieldException, IllegalAccessException {
+        shortUrlGeneratorService = new ShortUrlGeneratorService(stringShortener);
+        Field hostname = shortUrlGeneratorService.getClass().getDeclaredField("hostname");
+        hostname.setAccessible(true);
+        hostname.set(shortUrlGeneratorService, hostnameValue);
+    }
+
+    @Test
+    void shouldReturnTrueIfGeneratesUrlCorrectly() {
+        String url = "http://google.com";
+        String mockedShortenedString = "12abc";
+        Mockito.when(stringShortener.shortenString(url)).thenReturn(mockedShortenedString);
+
+        String shortUrl = shortUrlGeneratorService.generateShortUrl(url);
+        String expectedUrl = "http://" + hostnameValue + "/l/" + mockedShortenedString;
+        assertEquals(expectedUrl, shortUrl);
+    }
+}
