e5e8fbe0c36d3e4d4e0007ef777d8d1389669ed6
kirillbowkin <kirillshishkin77@gmail.com>
2022-10-18 12:30:59 +0300

    add @Service to UrlBuilder

--- src/main/java/kirill/bowkin/urlshortener/service/urlBuilder/UrlBuilder.java
+++ src/main/java/kirill/bowkin/urlshortener/service/urlBuilder/UrlBuilder.java
@@ -3,7 +3,9 @@ package kirill.bowkin.urlshortener.service.urlBuilder;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UrlBuilder {
 
     private static final Logger logger = LoggerFactory.getLogger(UrlBuilder.class);
