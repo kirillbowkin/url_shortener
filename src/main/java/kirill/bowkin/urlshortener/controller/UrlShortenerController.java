5ded7e39bbb8686c0922408be284249217934723
kirillbowkin <kirillshishkin77@gmail.com>
2022-10-18 10:01:02 +0300

    add logging for UrlShortenerController

--- src/main/java/kirill/bowkin/urlshortener/controller/UrlShortenerController.java
+++ src/main/java/kirill/bowkin/urlshortener/controller/UrlShortenerController.java
@@ -7,6 +7,8 @@ import kirill.bowkin.urlshortener.exception.UrlFailedToSaveException;
 import kirill.bowkin.urlshortener.service.shortUrlGenerator.ShortUrlGenerator;
 import kirill.bowkin.urlshortener.service.urlValidator.UrlValidator;
 import kirill.bowkin.urlshortener.service.urls.UrlsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +21,8 @@ public class UrlShortenerController {
     private final UrlValidator urlValidator;
     private final ShortUrlGenerator shortUrlGenerator;
     private final UrlsService urlsService;
+    private final Logger logger = LoggerFactory.getLogger(UrlShortenerController.class);
+
 
     public UrlShortenerController(UrlValidator urlValidator, ShortUrlGenerator shortUrlGenerator, UrlsService urlsService) {
         this.urlValidator = urlValidator;
@@ -33,13 +37,16 @@ public class UrlShortenerController {
         boolean isValid = urlValidator.validate(originalUrl);
 
         if(!isValid) {
+            logger.error("Url {} is invalid", originalUrl);
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid url");
         }
 
         try {
             UrlsEntity savedEntity = urlsService.save(originalUrl);
+            logger.info("Generated short url {} for {}", savedEntity.getShortUrl(), originalUrl);
             return new GenerateResponseDto(savedEntity.getShortUrl());
         } catch (UrlFailedToSaveException e) {
+            logger.info("Failed to save url {}", originalUrl);
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to save url", e.getCause());
         }
 
